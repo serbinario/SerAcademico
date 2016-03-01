023dacd23a8f1f16cedc51b2d4949c46468ec8e1
AndreySerbinario <andreyfbs2013@gmail.com>
2016-03-01 09:45:06 -0300

    Atualizações

--- src/Serbinario/Bundles/UtilBundle/Controller/UtilController.php
+++ src/Serbinario/Bundles/UtilBundle/Controller/UtilController.php
@@ -5,11 +5,70 @@ namespace Serbinario\Bundles\UtilBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Get;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UtilController extends FOSRestController
 {
+    /**
+     * @Post("/search")
+     */
+    public function searchAction(Request $request)
+    {
+        try {
+            #Recuperando os serviços
+            $serializer   = $this->get("jms_serializer");
+
+            #Recuperando dados da requisição
+            $search       = $request->request->get("search");
+            $tableName    = $request->request->get("tableName");
+            $fieldName    = $request->request->get("fieldName");
+            $page         = $request->request->get("page");
+
+            #Verificando se houve texto na para consulta
+            if(!$search) {
+                #Retorno
+                return new Response($serializer->serialize([], "json"));
+            }
+
+            #Recupearando o entity manager e a query builder
+            $manager      = $this->getDoctrine()->getManager();
+            $queryBuilder = $manager->createQueryBuilder();
+
+            #Consulta
+            $queryBuilder->select("a");
+            $queryBuilder->from("{$tableName}", "a");
+            $queryBuilder->where("a.{$fieldName} like '{$search}%'");
+
+            #Inicio da consulta
+            if($page) {
+                $queryBuilder->setFirstResult($page);
+            }
+
+            #Limite da consulta
+            $queryBuilder->setMaxResults(10);
+
+            #Executando a consulta
+            $resultQuery  = $queryBuilder->getQuery()->getResult();
+
+            #Preparando o retorno
+            $result = array();
+            foreach($resultQuery as $item) {
+                $result[] = array(
+                    "id" => $item->getId(),
+                    "text" => $item->getNome()
+                );
+            }
+
+            #Retorno
+            return new Response($serializer->serialize($result, "json"));
+        }  catch (\Throwable $e) {
+            #Retorno
+            return new Response($serializer->serialize($e->getMessage(), "json"));
+        }
+    }
+
     /**
      * @Post("/queryByField")
      */
